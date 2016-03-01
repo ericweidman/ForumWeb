63f982f4406575adb4ad6bc826233f741b4a3a47
ericweidman <ericweidman@gmail.com>
2016-03-01 11:39:06 -0500

    Added tests.

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,6 +5,7 @@ import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -13,7 +14,38 @@ public class Main {
     static HashMap<String, User> users = new HashMap<>();
     static ArrayList<Message> messages = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, password VARCHAR)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS messages (id IDENTITY, user_id INT, reply_id INT, text VARCHAR)");
+
+    }
+
+    public static void insertUser(Connection conn, String name, String password) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?, ?)");
+        stmt.setString(1, name);
+        stmt.setString(2, password);
+        stmt.execute();
+
+    }
+    public static User selectUser(Connection conn, String name) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE name = ?");
+        stmt.setString(1, name);
+        ResultSet results = stmt.executeQuery();
+        if(results.next()){
+            int id = results.getInt("id");
+            String password = results.getString("password");
+            return new User(id, name, password);
+        }
+        return null;
+
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+
         addTestUsers();
 	    addTestMessages();
 
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -4,11 +4,18 @@ package com.theironyard;
  * Created by zach on 2/24/16.
  */
 public class User {
+    int id;
     String name;
     String password;
 
-    public User(String name, String password) {
+    public User(int id, String name, String password) {
+        this.id = id;
         this.name = name;
         this.password = password;
+    }
+
+    public User(String password, String name) {
+        this.password = password;
+        this.name = name;
     }
 }
